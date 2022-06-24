5b1a830066a5bf4241c2316236cf75e030185d05
Jesyka607 <105829181+Jesyka607@users.noreply.github.com>
2022-06-24 07:25:34 +0100

    Add files via upload

---
+++ Simple.js
@@ -0,0 +1,7 @@
+//Write a javasript code that prints out your me, height and country.
+
+let myname ='JessicaAtufu';
+let myheight ='5ft6';
+let mycountry = 'Nigeria';
+document.write(myname + "<br>" + myheight + "<br>" + mycountry);
+
